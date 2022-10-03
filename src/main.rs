8e1fe467fe68dbb5fa3a1510deacab19f3f06d88
Dariusz Depta <dariusz.depta@dmntk.io>
2022-10-03 17:50:16 +0200

    Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -34,165 +34,212 @@
 
 extern crate ncurses;
 
+mod errors;
 mod keys;
 mod plane;
 #[cfg(test)]
 mod tests;
 
+use errors::*;
 use keys::*;
 use ncurses::*;
 use plane::*;
 use std::{env, fs};
 
-/// Initializes ncurses.
-fn initialize() -> WINDOW {
-  // set locale to Unicode en-US
-  let locale_conf = LcCategory::all;
-  setlocale(locale_conf, "en_US.UTF-8");
-  // start ncurses
-  let window = initscr();
-  // switch to raw mode
-  raw();
-  // allow for extended keyboard (like F1)
-  keypad(window, true);
-  // disable echo
-  noecho();
-  // return current window handler
-  window
+struct Editor {
+  /// Handle of the main window of the terminal.
+  window: WINDOW,
+  plane: Plane,
 }
 
-/// Terminates ncurses.
-fn finalize() {
-  // terminate ncurses
-  endwin();
-}
-
-/// Loads the input file.
-fn load_from_file(file_name: &str) -> Option<Plane> {
-  if let Ok(content) = fs::read_to_string(file_name) {
-    Some(Plane::new(&content))
-  } else {
-    eprintln!("Loading file failed: {}", file_name);
-    None
+impl Editor {
+  /// Creates a new editor initialized with the content loaded from file.
+  fn new(file_name: &str) -> Result<Self, AttoError> {
+    let content = fs::read_to_string(file_name).map_err(|e| err_load_file(file_name, &e.to_string()))?;
+    let plane = Plane::new(&content);
+    let window = Self::initialize();
+    Ok(Self { window, plane })
   }
-}
-
-fn repaint_plane(plane: &Plane) {
-  for (r, row) in plane.rows.iter().enumerate() {
-    mv(r as i32, 0);
-    addstr(&row.to_string());
-    addstr(" ");
+  /// Initializes terminal via ncurses.
+  fn initialize() -> WINDOW {
+    let locale_conf = LcCategory::all;
+    setlocale(locale_conf, "en_US.UTF-8");
+    let window = initscr();
+    raw();
+    keypad(window, true);
+    noecho();
+    window
   }
-  mv(plane.cur_screen_row(), plane.cur_screen_col());
-  refresh();
-}
-
-/// Processes input keystrokes.
-fn process_keystrokes(plane: &mut Plane, window: WINDOW) {
-  let mut cur_x = 0;
-  let mut cur_y = 0;
-  let mut max_x = 0;
-  let mut max_y = 0;
-  for row in &plane.rows {
+  /// Terminates terminal via ncurses.
+  fn finalize(&self) -> Result<()> {
+    endwin();
+    Ok(())
+  }
+  /// Updates cursor position.
+  fn update_cursor(&self) {
+    mv(self.plane.cur_screen_row(), self.plane.cur_screen_col());
+  }
+  /// Updates cursor coordinates in status bar.
+  fn update_cursor_coordinates(&self) {
+    let mut cur_x = 0;
+    let mut cur_y = 0;
+    let mut max_x = 0;
+    let mut max_y = 0;
+    getmaxyx(self.window, &mut max_y, &mut max_x);
+    getyx(self.window, &mut cur_y, &mut cur_x);
+    mvaddstr(
+      max_y - 1,
+      max_x - 20,
+      &format!("{:>20}", format!("{}:{} ", self.plane.cur_screen_col(), self.plane.cur_screen_row())),
+    );
     mv(cur_y, cur_x);
-    addstr(&row.to_string());
-    cur_y += 1;
   }
-  cur_x = plane.cur_screen_col();
-  cur_y = plane.cur_screen_row();
-  mv(cur_y, cur_x);
-  loop {
-    let ch = getch();
-    let key_name = keyname(ch).unwrap_or_default();
-    match key_name.as_str() {
-      KN_CTRL_Q => break,
-      KN_UP => {
-        if plane.move_up() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_DOWN => {
-        if plane.move_down() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_LEFT => {
-        if plane.move_left() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_RIGHT => {
-        if plane.move_right() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_BACKSPACE => {
-        plane.delete_character_before(true);
-        repaint_plane(plane);
-      }
-      KN_DELETE => {
-        plane.delete_character(true);
-        repaint_plane(plane);
-      }
-      KN_SHIFT_DELETE => {
-        plane.delete_character(false);
-        repaint_plane(plane);
-      }
-      KN_HOME => {
-        if plane.move_cell_start() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_END => {
-        if plane.move_cell_end() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_SHIFT_HOME => {
-        if plane.move_table_start() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_SHIFT_END => {
-        if plane.move_table_end() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_TAB => {
-        if plane.move_cell_next() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_SHIFT_TAB => {
-        if plane.move_cell_prev() {
-          mv(plane.cur_screen_row(), plane.cur_screen_col());
-        }
-      }
-      KN_RESIZE => {
-        getmaxyx(window, &mut max_y, &mut max_x);
-        // getyx(window, &mut cur_y, &mut cur_x);
-        // attron(A_REVERSE());
-        // mvaddstr(43, 1, &format!("{}:{}", max_x, max_y));
-        // attroff(A_REVERSE());
-        // mv(cur_y, cur_x);
-        // refresh();
-        a(window, max_x, max_y, plane.cur_screen_col(), plane.cur_screen_row());
-      }
-      _ => match ch {
-        32..=127 => {
-          if let Some(new_ch) = char::from_u32(ch as u32) {
-            plane.insert_character(new_ch);
-            repaint_plane(plane);
+  /// Repaints the content of a plane.
+  fn repaint_plane(&self) {
+    for (r, row) in self.plane.rows.iter().enumerate() {
+      mv(r as i32, 0);
+      addstr(&row.to_string());
+      addstr(" ");
+    }
+  }
+  /// Processes input keystrokes.
+  fn process_keystrokes(&mut self) {
+    let mut cur_x = 0;
+    let mut cur_y = 0;
+    let mut max_x = 0;
+    let mut max_y = 0;
+    for row in &self.plane.rows {
+      mv(cur_y, cur_x);
+      addstr(&row.to_string());
+      cur_y += 1;
+    }
+    self.update_cursor();
+    self.update_cursor_coordinates();
+    refresh();
+    loop {
+      let ch = getch();
+      let key_name = keyname(ch).unwrap_or_default();
+      match key_name.as_str() {
+        KN_CTRL_Q => break,
+        KN_UP => {
+          if self.plane.move_up() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
           }
         }
-        _ => {
-          getyx(window, &mut cur_y, &mut cur_x);
-          mvaddstr(40, 1, &format!("{:X}", ch));
-          mvaddstr(41, 1, &format!("{:40}", key_name));
-          mv(cur_y, cur_x);
+        KN_DOWN => {
+          if self.plane.move_down() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_LEFT => {
+          if self.plane.move_left() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_RIGHT => {
+          if self.plane.move_right() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_BACKSPACE => {
+          self.plane.delete_character_before(true);
+          self.repaint_plane();
+          self.update_cursor();
+          self.update_cursor_coordinates();
           refresh();
         }
-      },
+        KN_DELETE => {
+          self.plane.delete_character(true);
+          self.repaint_plane();
+          self.update_cursor();
+          self.update_cursor_coordinates();
+        }
+        KN_SHIFT_DELETE => {
+          self.plane.delete_character(false);
+          self.repaint_plane();
+          self.update_cursor();
+          self.update_cursor_coordinates();
+          refresh();
+        }
+        KN_HOME => {
+          if self.plane.move_cell_start() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_END => {
+          if self.plane.move_cell_end() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_SHIFT_HOME => {
+          if self.plane.move_table_start() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_SHIFT_END => {
+          if self.plane.move_table_end() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_TAB => {
+          if self.plane.move_cell_next() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_SHIFT_TAB => {
+          if self.plane.move_cell_prev() {
+            self.update_cursor();
+            self.update_cursor_coordinates();
+            refresh();
+          }
+        }
+        KN_RESIZE => {
+          getmaxyx(self.window, &mut max_y, &mut max_x);
+          // getyx(window, &mut cur_y, &mut cur_x);
+          // attron(A_REVERSE());
+          // mvaddstr(43, 1, &format!("{}:{}", max_x, max_y));
+          // attroff(A_REVERSE());
+          // mv(cur_y, cur_x);
+          // refresh();
+          a(self.window, max_x, max_y, self.plane.cur_screen_col(), self.plane.cur_screen_row());
+        }
+        _ => match ch {
+          32..=126 => {
+            if let Some(new_ch) = char::from_u32(ch as u32) {
+              self.plane.insert_character(new_ch);
+              self.repaint_plane();
+              self.update_cursor();
+              self.update_cursor_coordinates();
+              refresh();
+            }
+          }
+          _ => {
+            getyx(self.window, &mut cur_y, &mut cur_x);
+            mvaddstr(40, 1, &format!("{:X}", ch));
+            mvaddstr(41, 1, &format!("{:40}", key_name));
+            mv(cur_y, cur_x);
+            refresh();
+          }
+        },
+      }
     }
   }
 }
@@ -215,15 +262,13 @@ fn usage() {
 }
 
 /// Main entrypoint.
-fn main() {
+fn main() -> Result<()> {
   let args: Vec<String> = env::args().collect();
   if args.len() != 2 {
     usage();
-    return;
+    return Err(err_invalid_arguments());
   }
-  let window = initialize();
-  if let Some(mut plane) = load_from_file(&args[1]) {
-    process_keystrokes(&mut plane, window);
-  }
-  finalize();
+  let mut editor = Editor::new(&args[1])?;
+  editor.process_keystrokes();
+  editor.finalize()
 }
