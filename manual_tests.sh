e8de3f571e53df701f54caac37eed2ad14065b7d
Dariusz Depta <dariusz.depta@dmntk.io>
2022-10-06 14:31:39 +0200

    Code coverage.

--- manual_tests.sh
+++ manual_tests.sh
@@ -12,7 +12,51 @@ sleep 0.2
 
 # send unhandled key combinations to make debug message to be displayed
 tmux send-keys -t Atto C-M-End
-#tmux capture-pane -t Atto -p -S
+
+# move to cell end
+tmux send-keys -t Atto End
+
+# move to cell start
+tmux send-keys -t Atto Home
+
+# move down
+tmux send-keys -t Atto Down
+
+# move down
+tmux send-keys -t Atto Down
+
+# move up
+tmux send-keys -t Atto Up
+
+# move to table end
+tmux send-keys -t Atto S-End
+
+# move to table start
+tmux send-keys -t Atto S-Home
+
+# move to next cell
+tmux send-keys -t Atto Tab
+
+# move to previous cell
+tmux send-keys -t Atto BTab
+
+# move right
+tmux send-keys -t Atto Right
+
+# move left
+tmux send-keys -t Atto Left
+
+# delete character before cursor
+tmux send-keys -t Atto BSpace
+
+# insert two characters
+tmux send-keys -t Atto "UK"
+
+# delete character under cursor
+tmux send-keys -t Atto Delete
+
+# display content
+# tmux capture-pane -t Atto -p -S
 
 # Ctrl+q : close editor; session will be automatically closed
 tmux send-keys -t Atto C-q
--- src/tests/action_insert.rs
+++ src/tests/action_insert.rs
@@ -89,3 +89,32 @@ fn _0002() {
 "#;
   eq(plane, expected);
 }
+
+#[test]
+fn _0003() {
+  let plane = &mut Plane::new(TEST_INPUT_001);
+  eq_cursor(1, 1, plane);
+  repeat!(1, plane, cursor_move_down);
+  repeat!(2, plane, cursor_move_right);
+  plane.insert_char('K');
+  let expected = r#"
+┌─────────────────────────────────────┐
+│ Order options                       │
+├────┬───────────┬───────╥────────────┴────────╥─────────────┬───────────┐
+│ UK │           │       ║    Order options    ║             │           │
+│    │ Customer  │ Order ╟──────────┬──────────╢ Description │ Reference │
+│    │   type    │ size  ║ Discount │ Priority ║             │           │
+│    ├───────────┼───────╫──────────┼──────────╫─────────────┼───────────┤
+│    │"Business",│  <10, ║   0.10,  │"Normal", ║             │           │
+│    │"Private"  │ >=10  ║   0.15,  │ "High",  ║             │           │
+│    │           │       ║   0.05   │ "Low"    ║             │           │
+╞════╪═══════════╪═══════╬══════════╪══════════╬═════════════╪═══════════╡
+│ 1  │"Business" │  <10  ║   0.10   │ "Normal" ║ Small order │   Ref 1   │
+├────┼───────────┼───────╫──────────┼──────────╫─────────────┼───────────┤
+│ 2  │"Business" │ >=10  ║   0.15   │ "High"   ║ Large order │   Ref 2   │
+├────┼───────────┼───────╫──────────┼──────────╫─────────────┼───────────┤
+│ 3  │"Private"  │   -   ║   0.05   │ "Low"    ║ All orders  │   Ref 3   │
+└────┴───────────┴───────╨──────────┴──────────╨─────────────┴───────────┘
+"#;
+  eq(plane, expected);
+}
